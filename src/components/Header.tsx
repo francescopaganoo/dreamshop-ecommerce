7a4fdd69265df917b73332795328d84f355d4f43
francescopaganoo <paganofrancesco5@gmail.com>
2025-08-25 09:36:56 +0200

    modifica colore testo search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -124,7 +124,7 @@ export default function Header() {
               <input
                 type="text"
                 placeholder="Cerca..."
-                className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-bred-700"
+                className="w-full px-4 py-2 border text-white border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-bred-700"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
@@ -132,7 +132,7 @@ export default function Header() {
                 type="submit"
                 className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
               >
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg className="w-6 h-6 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
               </button>
@@ -249,7 +249,7 @@ export default function Header() {
                   <input
                     type="text"
                     placeholder="Cerca..."
-                    className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-bred-700"
+                    className="w-full px-4 py-2 text-white border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-bred-700"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                   />
