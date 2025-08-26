4e412780c05f01b0c57c6d477486033f4efff1a3
francescopaganoo <paganofrancesco5@gmail.com>
2025-08-26 10:34:34 +0200

    cambiato lingua da en a it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="it">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${bangers.variable} antialiased`}
       >
